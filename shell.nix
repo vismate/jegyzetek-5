ac36a0473ec0a1c459ae2613048ba17949d42dc1
vismate <visnyarmate@gmail.com>
2023-09-10 22:26:58 +0200

    fixed versions

--- shell.nix
+++ shell.nix
@@ -1,5 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let 
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+  }){};
+in
 pkgs.mkShell {
   name = "mdbook-env";
   buildInputs = with pkgs; [
